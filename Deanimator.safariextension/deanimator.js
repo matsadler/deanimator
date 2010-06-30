e291aa22bd4ecf3c2a6513f31418added44999f9
Matthew Sadler <mat@sourcetagsandcodes.com>
2010-06-30 18:33:11 +0100

    Figured out how to bypass the same origin policy and get data URLs for all images. Removed canvas background image trick.

--- Deanimator.safariextension/deanimator.js
+++ Deanimator.safariextension/deanimator.js
@@ -1,52 +1,25 @@
-function dataURLReplace(img) {
-    var canvas = document.createElement("canvas");
-        context = canvas.getContext("2d");
-    canvas.width = img.width;
-    canvas.height = img.height;
-    context.drawImage(img, 0, 0, img.width, img.height);
-    img.src = canvas.toDataURL();
-}
+var map = {}
 
-var UID = (function () {
-    var count = -1;
-    return function () {
-        return (count += 1);
-    };
-})();
+safari.self.addEventListener("message", function (event) {
+    var name = event.name,
+        image = map[name];
+    console.log(name);
+    if (image) {
+        image.src = event.message;
+        delete map[name]
+    }
+});
 
-function getPadding(img) {
-    var style = document.defaultView.getComputedStyle(img),
-        paddingLeft = parseInt(style.getPropertyValue("padding-left"), 10),
-        paddingTop = parseInt(style.getPropertyValue("padding-top"), 10);
-    return {top: paddingTop, left: paddingLeft};
-}
-
-function updateBackground(img) {
-    var id = "deanimator-image-" + UID(),
-        padding = getPadding(img),
-        ctx = document.getCSSCanvasContext("2d", id, padding.left, padding.top);
-    ctx.drawImage(img, 0, 0);
-    img.style.backgroundImage = "-webkit-canvas(" + id + ")";
-}
-
-function backgroundReplace(img) {
-    var id = "deanimator-image-" + UID(),
-        context = document.getCSSCanvasContext("2d", id, img.width, img.height),
-        padding = getPadding(img);
-    context.drawImage(img, padding.left, padding.top, img.width, img.height);
-    img.style.paddingLeft = img.width + padding.left + "px";
-    img.style.paddingTop = img.height + padding.top + "px";
-    img.style.width = "0";
-    img.style.height = "0";
-    img.style.backgroundImage = "-webkit-canvas(" + id + ")";
-    img.style.backgroundRepeat = "no-repeat";
-    img.addEventListener("load", function (event) {
-        updateBackground(event.target);
-    });
+function deanimate(image) {
+    var url = image.src,
+        eventName = "toDataURL: " + url;
+    map[eventName] = image;
+    safari.self.tab.dispatchMessage(eventName, url);
 }
 
 function withImages(func) {
     safari.self.addEventListener("message", function (event) {
+        console.log("hello");
         if (event.name === "applyToAll" && event.message) {
             func(document.images);
         } else if (event.name === "applyToAll") {
@@ -56,29 +29,15 @@ function withImages(func) {
     safari.self.tab.dispatchMessage("applyToAll");
 }
 
-function onComplete(img, func) {
-    if (img.complete) {
-        func(img);
-    } else {
-        img.addEventListener("load", function (event) {
-            func(event.target);
-        });
-    }
-}
-
 window.addEventListener("DOMContentLoaded", function () {
     var host = new RegExp("^(" + location.protocol + ")?//" + location.host);
     
     withImages(function (images) {
-        var image = null,
-            i;
+        var i;
         for (i = 0; i < images.length; i += 1) {
-            image = images[i];
-            if (image.src.match(host)) {
-                onComplete(image, dataURLReplace);
-            } else if (!image.useMap && !image.style.backgroundImage) {
-                onComplete(image, backgroundReplace);
-            }
+            images[i].addEventListener("load", function (event) {
+                deanimate(event.target);
+            });
         }
     });
 });
