a7f73d17170cd3d51dbca99ff58153df21a4fb5e
Matthew Sadler <mat@sourcetagsandcodes.com>
2010-08-16 22:07:19 +0100

    simplified

--- Deanimator.safariextension/deanimator.js
+++ Deanimator.safariextension/deanimator.js
@@ -1,52 +1,33 @@
-var grouped;
+var imagesBySrc;
 
 safari.self.addEventListener("message", function (event) {
     if (event.name.match(/^https?:\/\//)) {
-        var images = grouped[event.name], i;
+        var images = imagesBySrc[event.name], i;
         if (images) {
             for (i = 0; i < images.length; i += 1) {
                 images[i].src = event.message;
             }
         }
-        delete grouped[event.name];
+        delete imagesBySrc[event.name];
     }
 });
 
-function withImages(func) {
-    var callback = function (event) {
-        if (event.name === "shouldApplyToAll") {
-            if (event.message) {
-                func(document.images);
-            } else {
-                func(document.querySelectorAll('img[src$=".gif"]'));
-            }
-            safari.self.removeEventListener("message", callback);
-        }
-    };
-    
-    safari.self.addEventListener("message", callback);
-    safari.self.tab.dispatchMessage("shouldApplyToAll");
-}
-
-function groupBy(property, toGroup) {
-    var map = {}, obj, i;
-    for (i = 0; i < toGroup.length; i += 1) {
-        obj = toGroup[i];
-        (map[obj[property]] || (map[obj[property]] = [])).push(obj);
+function groupBy(property, array) {
+    var hash = {}, obj, i;
+    for (i = 0; i < array.length; i += 1) {
+        obj = array[i];
+        (hash[obj[property]] || (hash[obj[property]] = [])).push(obj);
     }
-    return map;
+    return hash;
 }
 
 window.addEventListener("DOMContentLoaded", function () {
-    withImages(function (images) {
-        var property, group;
-        grouped = groupBy("src", images);
-        
-        for (property in grouped) {
-            if (grouped.hasOwnProperty(property)) {
-                group = grouped[property];
-                safari.self.tab.dispatchMessage("toDataURL", group[0].src);
-            }
+    var url;
+    imagesBySrc = groupBy("src", document.images);
+    
+    for (url in imagesBySrc) {
+        if (imagesBySrc.hasOwnProperty(url)) {
+            safari.self.tab.dispatchMessage("toDataURL", url);
         }
-    });
+    }
 });
